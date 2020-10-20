e18b7f35435c3fcaf7e0afb815e975033b2c2c4d
Nordtwig <nordqvist.gaming@gmail.com>
2020-10-20 15:24:57 +0200

    Finished Modal stuff.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,13 +3,27 @@ import MobileMenu from "./modules/MobileMenu";
 import StickyHeader from "./modules/StickyHeader";
 import RevealOnScroll from "./modules/RevealOnScroll";
 
-let mobileMenu = new MobileMenu();
-let stickyHeader = new StickyHeader();
+new MobileMenu();
+new StickyHeader();
+
+let modal;
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
-
+document.querySelectorAll(".open-modal").forEach(element => {
+    element.addEventListener("click", event => {
+        event.preventDefault();
+        if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */ "./modules/Modal").then(x => {
+                modal = new x .default();
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log("There was a problem!"));
+        } else {
+            modal.openTheModal();
+        }
+    })
+});
 
 if (module.hot) {
     module.hot.accept();
